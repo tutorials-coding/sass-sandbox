e7887766977f426694566fd340b25cd1bd85cc58
Vadim <korobeinikovv@mail.ru>
2021-06-21 22:36:14 +0300

    Add card showroom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Button } from './components/Button'
+import { CardShowroom } from './components/Card/CardShowroom'
 
 import './App.css'
 
@@ -8,18 +9,27 @@ function App() {
   }
 
   return (
-    <div className="main-container">
-      <Button title="Confirm changes" onClick={handleClick} size="sm" />
-      <Button title="Confirm changes" onClick={handleClick} size="md" />
-      <Button title="Confirm changes" onClick={handleClick} size="lg" />
+    <>
+      <div className="main-container">
+        <Button title="Confirm changes" onClick={handleClick} size="sm" />
+        <Button title="Confirm changes" onClick={handleClick} size="md" />
+        <Button title="Confirm changes" onClick={handleClick} size="lg" />
 
-      <Button title="Confirm changes" onClick={handleClick} type="info" />
-      <Button title="Confirm changes" onClick={handleClick} type="success" />
-      <Button title="Confirm changes" onClick={handleClick} type="error" />
+        <Button title="Confirm changes" onClick={handleClick} type="info" />
+        <Button title="Confirm changes" onClick={handleClick} type="success" />
+        <Button title="Confirm changes" onClick={handleClick} type="error" />
 
-      <Button title="Confirm changes" onClick={handleClick} disabled />
-      <Button title="Confirm changes" onClick={handleClick} disabled={false} />
-    </div>
+        <Button title="Confirm changes" onClick={handleClick} disabled />
+        <Button
+          title="Confirm changes"
+          onClick={handleClick}
+          disabled={false}
+        />
+      </div>
+      <div className="main-container">
+        <CardShowroom />
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Card/CardShowroom.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { Card } from './Card'
+
+export function CardShowroom() {
+  const [theme, setTheme] = useState('theme1')
+
+  const toggleTheme = () => {
+    setTheme(theme === 'theme1' ? 'theme2' : 'theme1')
+  }
+
+  useEffect(() => {
+    const backgroundColor = `var(--background-color-${theme})`
+    const fontColor = `var(--font-color-${theme})`
+    document.body.style.setProperty('--background-color', backgroundColor)
+    document.body.style.setProperty('--font-color', fontColor)
+  }, [theme])
+
+  return (
+    <div>
+      <Card title="This is a title" subtitle="This is a subtitle." />
+
+      <button type="button" onClick={toggleTheme}>
+        Toggle theme (current theme: {theme})
+      </button>
+    </div>
+  )
+}
